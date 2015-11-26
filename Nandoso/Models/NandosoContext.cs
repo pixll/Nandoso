d9307b072c011dd5ce93722dd728d6596720810b
Jun Ruan <gmarshalmil@gmail.com>
2015-11-27 10:39:23 +1300

    Jquery updated

--- Nandoso/Models/NandosoContext.cs
+++ Nandoso/Models/NandosoContext.cs
@@ -7,6 +7,7 @@ using System.Data.Entity.Migrations;
 
 namespace Nandoso.Models
 {
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
     public class NandosoContext : DbContext
     {
         // You can add custom code to this file. Changes will not be overwritten.
